78097d742e4021eb38ec43f56c6712b8cddfbbca
Pourya <pouryazeidi@yahoo.com>
2024-08-31 01:50:02 +0300

    builed introduce section and make it responsive

---
+++ src/components/Introducing.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Introducing = () => {
+  return (
+    <div className='mt-5 w-full relative flex flex-col md:flex-row justify-between h-[100vh]'>
+
+      <img 
+        src="/itroduc1.png" 
+        alt="" 
+        className='w-full md:w-[49%] h-[50vh] object-cover' 
+      />
+
+      <img 
+        src="/itroduc2.png" 
+        alt="" 
+        className='hidden md:flex w-full md:w-[49%] h-[50vh] object-cover' 
+      />
+
+      <div 
+        className='absolute w-[250px] h-[380px] md:h-[500px] md:w-[380px] lg:w-[400px] lg:h-[600px] left-5 sm:left-20 top-40 bg-gray-400 opacity-85 rounded-xl'
+      >
+        <img 
+          src="/man1.jpg" 
+          alt="man office" 
+          className='rounded-[50%] w-[165px] h-[140px] sm:w-[175px] sm:h-[140px] md:w-[200px] md:h-[175px] lg:w-[250px] lg:h-[230px] absolute left-[45px] sm:left-11 md:left-[85px] lg:left-20 top-16' 
+        />
+        
+        <div className='absolute flex justify-center flex-col h-[50%]  items-center bottom-0 text-center'>
+          <h3 className='font-bold text-[18px] sm:text-[20px] md:text-[24px] lg:text-[28px]'>
+            Benjamin Shah
+          </h3>
+          <p className='font-semibold text-[16px] sm:text-[18px] md:text-[20px] lg:text-[22px]'>
+            CEO
+          </p>
+          <p className='flex justify-center text-[14px] sm:text-[16px] md:text-[18px] lg:text-[20px] p-2'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae adipisci odio officiis blanditiis.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Introducing;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import HeroSection from "@/components/HeroSection";
+import Introducing from "@/components/Introducing";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <main >
       <HeroSection/>
+      <Introducing/>
     </main>
   );
 }
